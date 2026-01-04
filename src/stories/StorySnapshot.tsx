97c416f9fa00da35d304bf0eb1f38f971ca540ef
Max Donchenko <maxx.donchenko@gmail.com>
2026-01-04 03:08:47 +0200

    wire storybook actions and game renderer

--- src/stories/StorySnapshot.tsx
+++ src/stories/StorySnapshot.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import { SettingsProvider } from '@/context/SettingsContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { GameRenderer } from '@/components/GameRenderer/GameRenderer';
@@ -6,6 +7,7 @@ import { deserializeGameState } from '@/utils/gamePersistence';
 import type { SerializableGameState } from '@/utils/gamePersistence';
 import type { Card as CardType } from '@/game/Card';
 import type { WildValue } from '@/types/Card.types';
+import type { Coordinate } from '@/types/Grid.types';
 
 interface StorySnapshotProps {
   gameState: SerializableGameState;
@@ -22,7 +24,18 @@ export function StorySnapshot({ gameState: serializedState }: StorySnapshotProps
     Array<{ card: CardType; position: { x: number; y: number }; wildValue?: WildValue }>
   >([]);
 
-  const noop = () => {};
+  // Wire up Storybook actions for all GameRenderer props
+  const actions = {
+    onSelectCard: action('onSelectCard') as (card: CardType, index: number) => void,
+    onPlaceCard: action('onPlaceCard') as (position: Coordinate) => void,
+    onConfirmTurn: action('onConfirmTurn'),
+    onCancelPreview: action('onCancelPreview'),
+    onDiscard: action('onDiscard'),
+    onPass: action('onPass'),
+    onNewGame: action('onNewGame'),
+    onExport: action('onExport'),
+    onImport: action('onImport'),
+  };
 
   return (
     <SettingsProvider>
@@ -34,15 +47,7 @@ export function StorySnapshot({ gameState: serializedState }: StorySnapshotProps
           nextCardIndex={-1}
           isMyTurn={true}
           isAITurn={false}
-          onSelectCard={noop}
-          onPlaceCard={noop}
-          onConfirmTurn={noop}
-          onCancelPreview={noop}
-          onDiscard={noop}
-          onPass={noop}
-          onNewGame={noop}
-          onExport={noop}
-          onImport={noop}
+          {...actions}
         />
       </ThemeProvider>
     </SettingsProvider>
